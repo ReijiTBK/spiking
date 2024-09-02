6554c069393f35366b02cf4744a96c10cece6809
ReijiTBK <71742971+ReijiTBK@users.noreply.github.com>
2024-09-02 11:44:14 +0800

    Changing Port number

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8000;
 const POST_URI = process.env.POST_URI
 app.get('/', (req, res) => {
   res.send('Hello, World!');
