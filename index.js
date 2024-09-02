0cc459fa85d5f53f18fc574be1d2619ae105250f
ReijiTBK <71742971+ReijiTBK@users.noreply.github.com>
2024-09-02 11:53:22 +0800

    First FormSG Test

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-const express = require('express');
-const app = express();
-const port = 8000;
-const POST_URI = process.env.POST_URI
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+const express = require('express')
+const app = express()
 
-app.post('/', (req, res)=>{
-    console.log("request received");
-    console.log("========================");
-    console.log(req);
-});
+// Instantiating formsg-sdk without parameters default to using the package's
+// production public signing key.
+const formsg = require('@opengovsg/formsg-sdk')()
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-  console.log(POST_URI);
-});
+// This is where your domain is hosted, and should match
+// the URI supplied to FormSG in the form dashboard
+// const POST_URI = 'https://my-domain.com/submissions'
+const POST_URI = process.env.POST_URI;
+
+// Your form's secret key downloaded from FormSG upon form creation
+const formSecretKey = process.env.FORM_SECRET_KEY
+
+// Set to true if you need to download and decrypt attachments from submissions
+const HAS_ATTACHMENTS = false
+
+app.post(
+  '/submissions',
+  // Endpoint authentication by verifying signatures
+  function (req, res, next) {
+    try {
+      formsg.webhooks.authenticate(req.get('X-FormSG-Signature'), POST_URI)
+      // Continue processing the POST body
+      return next()
+    } catch (e) {
+      return res.status(401).send({ message: 'Unauthorized' })
+    }
+  },
+  // Parse JSON from raw request body
+  express.json(),
+  // Decrypt the submission
+  async function (req, res, next) {
+    // If `verifiedContent` is provided in `req.body.data`, the return object
+    // will include a verified key.
+    const submission = HAS_ATTACHMENTS
+      ? await formsg.crypto.decryptWithAttachments(formSecretKey, req.body.data)
+      : formsg.crypto.decrypt(formSecretKey, req.body.data)
+
+    // If the decryption failed, submission will be `null`.
+    if (submission) {
+        console.log(submission)
+      // Continue processing the submission
+    } else {
+      // Could not decrypt the submission
+    }
+  }
+)
+
+app.listen(8000, () => console.log('Running on port 8000'))
