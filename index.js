c1ec1c58d795ba00cadb7ab7a8df1e551183b053
ReijiTBK <71742971+ReijiTBK@users.noreply.github.com>
2024-10-25 00:01:14 +0800

    Fixed another small typo

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post(
   async function (req, res, next) {
     // If `verifiedContent` is provided in `req.body.data`, the return object
     // will include a verified key.
-    formSecretKey = mformSecretyKey[req.body.data.formId]
+    formSecretKey = mformSecretKey[req.body.data.formId]
     console.log("What is the Body? ",req.body.data);
     const submission = HAS_ATTACHMENTS
       ? await formsg.crypto.decryptWithAttachments(formSecretKey, req.body.data)
